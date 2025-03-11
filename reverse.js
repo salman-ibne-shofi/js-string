2be191e696ccf281b516492545bfd4c2e6ca8d56
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-12 00:32:43 +0600

    reverse a string in many difference way

---
+++ reverse.js
@@ -0,0 +1,32 @@
+const sentence = "I am learning web dev";
+// const result = ".ved bew gninrael ma i";
+
+// web
+// bew
+// donal
+// d
+// od
+// nod
+// anod
+// lanod
+
+let reverse = "";
+for (const letter of sentence) {
+	// console.log(letter);
+	reverse = letter + reverse;
+}
+// console.log(reverse);
+
+let rev = "";
+for(let i = 0; i < sentence.length; i++){
+    // console.log(i);
+    // console.log(sentence[i]);
+    const letter = sentence[i];
+    rev = letter + rev;
+}
+// console.log(rev);
+
+// shortcut
+const reversed = sentence.split("").reverse().join("");
+console.log(reversed);
+

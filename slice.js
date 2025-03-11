036e4691b5dde1936b5d8584cbf60d0f8d1d5dd6
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-12 00:08:13 +0600

    slice join

---
+++ slice.js
@@ -0,0 +1,17 @@
+const address = 'shibbari';
+const part = address.slice(2,5)
+// console.log(part);
+
+const sentence = 'I am a good and hard working person.';
+// console.log(sentence.split(' '));
+// console.log(sentence.split('a'));
+
+const friendStr = 'Rahim,Kahim,Fahim,Dahim,Lahim,Fahim,Sahim';
+const friends = friendStr.split(',');
+// console.log(friends);
+
+const realFriend = ['Rahim', 'Kahim', 'Dahim', 'Lahim', 'Fahim'];
+// console.log(realFriend.join());
+// console.log(realFriend.join('|'));
+// console.log(realFriend.join('-'));
+

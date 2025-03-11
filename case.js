e71e2f753286abb74e3fdcb74a15883fbb6e9062
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-11 23:53:41 +0600

    lowercase uppercase and trim

---
+++ case.js
@@ -0,0 +1,26 @@
+const school = "Rajuk Uttra Model School";
+
+// console.log(school);
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+// uppercase: ABCD
+// lowercase: abcd
+
+const subject = "Chemistry";
+const book = "chemIsTry";
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+	console.log("I am reading book");
+} else {
+	console.log("Hudai hudai");
+}
+
+const drink = " water";
+const liquid = "water ";
+
+if (drink.trim() === liquid.trim()) {
+	console.log("pani er opor nam life");
+} else {
+	console.log("salt water");
+}

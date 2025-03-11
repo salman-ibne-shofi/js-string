3f7b8ef500726b0a9311365c365cf80173af7263
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-03-12 00:50:23 +0600

    introduction to object properties primitive and non-primitive

---
+++ object.js
@@ -0,0 +1,33 @@
+// primitive
+// basic, primary
+const age = 21;
+const school = "PMG High School";
+const isPassed = true;
+// let isDeveloper;
+
+const subjects = ["Bangla", "English", "Physics", "Math"];
+const bottle_wrong = ["white", 45];
+const bottleColor = "white";
+const bottlePrice = 45;
+const bottleDrinks = "water";
+
+// object
+// non-primitive
+const bottle = {
+	brand: "apple",
+	price: 45,
+	color: "white",
+	isClear: false,
+};
+
+// non-primitive
+const subject = {
+	name: "Biology",
+	teacher: "Rasheda Mam",
+	date: "30 Feb",
+	chapters: ["first", "second", "third"],
+	exams: {
+		name: "Final Exam",
+		marks: 100,
+	},
+};
